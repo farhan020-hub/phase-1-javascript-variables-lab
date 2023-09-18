d5fa3a23709bfb3c7dfa1d5e27e398a576899f61
AbdiazizSaman <farhanadam394@gmail.com>
2023-09-18 10:47:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+
+let companyName = 'Scuber';
+
+companyName;
+
+function companyName(){
+    const companyName = "Scuber"
+};
+
+
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
+
+
+
